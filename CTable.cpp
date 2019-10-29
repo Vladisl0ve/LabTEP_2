c4c1707c3b375896673acbbb1c3c67efbd37c69a
Vladislove <vladislav.lapko@gmail.com>
2019-10-29 15:31:14 +0100

    Changed bSetNewSize()

--- CTable.cpp
+++ CTable.cpp
@@ -19,7 +19,12 @@ CTable::CTable(string s_Name, int iTableLen)
 CTable::CTable(const CTable& pcOther)
 {
 	sName = pcOther.sName + "_copy";
-	piTable = pcOther.piTable;
+	iSize = pcOther.iSize;
+	piTable = new int[pcOther.iSize];
+	for (int ii = 0; ii < iSize; ii++)
+	{
+		piTable[ii] = pcOther.piTable[ii];
+	}
 	cout << "Kopuj: " << sName << endl;
 }
 
@@ -35,6 +40,23 @@ bool CTable::bSetNewSize(int iTableLen)
 		return false;
 
 	int* pi_newTable = new int[iTableLen];
+
+
+	if (iSize < iTableLen)
+	{
+		for (int ii = 0; ii < iSize; ii++)
+		{
+			pi_newTable[ii] = piTable[ii];
+		}
+	}
+	else
+	{
+		for (int ii = 0; ii < iTableLen; ii++)
+		{
+			pi_newTable[ii] = piTable[ii];
+		}
+	}
+	delete piTable;
 	iSize = iTableLen;
 	piTable = pi_newTable;
 	return true;
@@ -60,13 +82,13 @@ bool CTable::bCheckErr(int iCheackable)
 	if (iCheackable < 0)
 	{
 		cout << "Error, new size must be greater than 0" << endl;
-		cout << iCheackable << " is less than zero" << endl;
+		cout << iCheackable << " is less than 0" << endl;
 		return false;
 	}
 	if (iCheackable == 0)
 	{
 		cout << "Error, new size must be greater than 0" << endl;
-		cout << iCheackable << " is equal to zero" << endl;
+		cout << iCheackable << " is equal to 0" << endl;
 		return false;
 	}
 	return true;
@@ -77,16 +99,16 @@ bool CTable::bCheckErrForNewTable(int iCheackable)
 	if (!bCheckErr(iCheackable))
 		return false;
 
-	if (iCheackable <= iSize)
+	/*if (iCheackable <= iSize)
 	{
 		cout << "Error, new size must be greater than previous size" << endl;
 		cout << iCheackable << " is less than previous size" << endl;
 		return false;
-	}
+	}*/
 	if (iCheackable == iSize)
 	{
-		cout << "Error, new size must be greater than previous size" << endl;
-		cout << iCheackable << " is equal to previous size" << endl;
+		//cout << "Error, new size must be greater than previous size" << endl;
+		cout << "ERROR: " << iCheackable << " is equal to previous size" << endl;
 		return false;
 	}
 	return true;
@@ -94,16 +116,14 @@ bool CTable::bCheckErrForNewTable(int iCheackable)
 
 void CTable::v_mod_tab(CTable* pcTab, int iNewSize)
 {
-	if (!bCheckErrForNewTable(iNewSize))
-		return;
+
 
 	pcTab->bSetNewSize(iNewSize);
 }
 
 void CTable::v_mod_tab(CTable cTab, int iNewSize)
 {
-	if (!bCheckErrForNewTable(iNewSize))
-		return;
+
 	cTab.bSetNewSize(iNewSize);
 }
 
--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
 #include "CTable.h"
 
 /*TO DO:
-	DONE: 1. Remove "waste-code" for testing
+	DONE: 1. Remove "waste-code"
 	DONE: 2. Add #pragma once (and read about that!)
 	DONE: 3. Fix deallocating (add full-dealloc including nullptr)
 	DONE: 4. Check all returns
@@ -11,19 +11,19 @@
 int main()
 {
 	CTable testBezparm;
-	testBezparm.vShowSize();
 	CTable testParm("TestParam", 5);
 	CTable testKopuj(testBezparm);
 	cout << "#########\n" << endl;
 	CTable* pc_new_tab;
 	pc_new_tab = testBezparm.pcClone();
 	cout << "#########\n" << endl;
-	testBezparm.vShowSize();
+	cout << "Size before function: "; testBezparm.vShowSize();
 	testBezparm.v_mod_tab(testBezparm, 300);
-	testBezparm.vShowSize();
+	cout << "Size after function: "; testBezparm.vShowSize();
 	cout << "#########\n" << endl;
+	cout << "Size before function: "; testBezparm.vShowSize();
 	testBezparm.v_mod_tab(&testBezparm, 50);
-	testBezparm.vShowSize();
+	cout << "Size after function: "; testBezparm.vShowSize();
 	cout << "#########\n" << endl;
 
 	system("pause");
